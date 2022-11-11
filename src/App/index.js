fbcf2edf95bba0e845afdfff6dfd018a04e53df4
Karla Sequen <karla.sequen@megapaca.com>
2022-11-11 16:46:07 -0600

    delete unused folders and file, and add little about me

---
+++ src/App/index.js
@@ -0,0 +1,52 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="section-container">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="section-container-spacer text-center">
+              <h1 class="h2">About me</h1>
+            </div>
+
+            <div class="row custom-text-size">
+              <div class="col-md-10 col-md-offset-1">
+                <div class="row">
+                  <div class="col-xs-12 col-md-6">
+                    <h3></h3>
+                    <p>I'm a Full Stack Developer with 10+ years of experience developing web
+                      application with
+                      AngularJS and Angular 2+. I have experience develop in the backend API's Rest
+                      with
+                      Java8+, Kotlin, using Spring Framework with Hibernate, and using PostgreSQL
+                      and SQL Server as a database. (I also used MySQL and MongoDB but only in my
+                      local machine to test it) </p>
+                    <h3></h3>
+                    <p>I consider myself a self-taught person, I love learning and trying new
+                      technologies and
+                      I am a person who likes to learn something new every day,
+                    </p>
+                    <p>I love to solve real life problems using technology</p>
+                    <h3></h3>
+                    <p>
+                      If you like my profile and you want to know more about me, please contact me to
+                      hi@karlasequen.com</p>
+
+                      <h1>WORK IN PROGRESS...!!! please be patient :)</h1>
+                  </div>
+                  <div class="col-xs-12 col-md-6">
+                    {/* <img src="./assets/images/profil.jpg" class="img-responsive" alt="" /> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
