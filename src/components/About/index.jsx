41fb4c33a057219618193a83e8cae8db8e2a716c
Karla Sequen <karlasequen@gmail.com>
2023-07-13 22:11:22 -0600

    change about component to use tailwind

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,32 +1,42 @@
 import React from 'react'
-import styles from "./styles.module.css"
 
 export const About = () => {
     return (
-        <section className={styles["about-section"]}>
-            <div className={styles["about-description"]}>
-
-                <h1>About me</h1>
-                <p>I'm a Software Engineer with 12+ years of experience developing web
-                    application with
-                    AngularJS and Angular 2+. I have experience develop in the backend API's Rest
-                    with
-                    Java8+, Kotlin, using Spring Boot with Hibernate, and using PostgreSQL
-                    and SQL Server as a database. (I also used MySQL and MongoDB but only in my
-                    local machine to test it) </p>
-                <p>I consider myself a self-taught person, I love learning and trying new
-                    technologies and
-                    I am a person who likes to learn something new every day,
-                </p>
-                <p>I really enjoy solving problems through technology.</p>
-                <p>
-                    If you like my profile and you want to know more about me, please contact me to
-                    <span className={styles["about-description-email"]}> <a href="mailto:hi@karlasequen.com?subject=contact">hi@karlasequen.com</a></span>
-                </p>
+        <div className="container mx-auto px-4 py-8 mt-16">
+            <div className="flex items-center justify-center lg:justify-start">
+                <div className="w-full lg:w-2/3 flex items-center">
+                    <div className="mr-8">
+                        <h2 className="text-3xl font-bold mb-4 text-left">About me</h2>
+                        <p className="text-gray-700 mb-4">
+                            I'm a Software Engineer with 12+ years of experience developing web
+                            application with
+                            AngularJS and Angular 2+. I have experience develop in the backend API's Rest
+                            with
+                            Java8+, Kotlin, using Spring Boot with Hibernate, and using PostgreSQL
+                            and SQL Server as a database. (I also used MySQL and MongoDB but only in my
+                            local machine to test it) </p>
+                        <p className="text-gray-700 mb-4">
+                            I consider myself a self-taught person, I love learning and trying new
+                            technologies and
+                            I am a person who likes to learn something new every day,
+                        </p>
+                        <p className="text-gray-700 mb-4">
+                            I really enjoy solving problems through technology.
+                        </p>
+                        <p className="text-gray-700 mb-4">
+                            If you like my profile and you want to know more about me, please contact me to
+                            <span> <a href="mailto:hi@karlasequen.com?subject=contact">hi@karlasequen.com</a></span>
+                        </p>
+                    </div>
+                    <div className="rounded-full overflow-hidden mx-auto lg:ml-auto">
+                        <img
+                            src={'/assets/images/profil.jpg'}
+                            alt="Imagen"
+                            className="w-full h-full object-cover rounded-full"
+                        />
+                    </div>
+                </div>
             </div>
-            <div className={styles["about-image"]}>
-                <img src={'/assets/images/profil.jpg'} className="img-responsive" alt=""/>
-            </div>
-        </section>
+        </div>
     )
 }
